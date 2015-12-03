ce5fb3c322014306a29fb7a1ccda5bd451b996e5
Jeffrey Jen <yihungjen@gmail.com>
2015-12-03 06:50:02 +0000

    NIT: house keeping

--- Slack.js
+++ Slack.js
@@ -1,44 +1,12 @@
 "use strict";
 
-var https = require("https"),
-    Mustache = require("mustache"),
-    Promise = require("promise"),
-    url = require("url");
+var Promise = require("promise");
 
 var setting = require("./lib/config.v3"),
     Config = setting.Config;
 
-function push2slack(opts, tmpl, channel, msg) {
-    function doPush2Slack(ok, grr) {
-        var req = https.request(opts);
-        req.on("end", function() {
-            ok();
-        });
-        req.on("error", function(e) {
-            grr(e.message);
-        });
-
-        var message = JSON.parse(msg),
-            decodedSrc = new Buffer(message.userMetadata.src, "base64");
-
-        message.userMetadata.src = decodedSrc;
-        var output = {
-            text: Mustache.render(tmpl, message),
-            channel: channel,
-        };
-
-        req.write(JSON.stringify(output));
-        req.end();
-    }
-    return new Promise(doPush2Slack);
-}
-
-function makeSlackReq(slack_url) {
-    var slack_req_opts = url.parse(slack_url);
-    slack_req_opts.method = "POST";
-    slack_req_opts.headers = {"Content-Type": "application/json"};
-    return slack_req_opts;
-}
+var makeSlackReq = require("./lib/slack"),
+    push2slack = require("./lib/push2slack");
 
 exports.push = function(event, context) {
     var candidate = [];
